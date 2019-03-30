d3e5bd55295652a5915ac8874ccd515fb017c4ab
Christian Häge <christian.haege@gmx.net>
2019-03-30 17:56:30 +0100

    Added Visual studio project and test project

---
+++ p12-euler/p12-euler.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+
+
+int main(int argc, char **argv)
+{
+	unsigned long long p12_answ = 0;
+
+
+
+	std::cout << "The answer to the 12th problem of ProjectEuler.Net is " << p12_answ << ".\n";
+
+
+	return 0;
+}
